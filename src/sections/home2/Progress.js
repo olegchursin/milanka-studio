f6494fe94a862edc3464f6741f645172eb2de5f7
olegchursin <olegchursin@gmail.com>
2021-05-09 22:25:29 -0400

    adjust step by step guide content

--- src/sections/home2/Progress.js
+++ src/sections/home2/Progress.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
-import imgA1 from "../../assets/image/home-2/png/arrow-1.png";
-import imgA2 from "../../assets/image/home-2/png/arrow-2.png";
-import imgS from "../../assets/image/home-2/png/search.png";
-import imgE from "../../assets/image/home-2/png/event-confirm.png";
-import imgR from "../../assets/image/home-2/png/right-layer.png";
+import imgA1 from '../../assets/image/home-2/png/arrow-1.png';
+import imgA2 from '../../assets/image/home-2/png/arrow-2.png';
+import imgS from '../../assets/image/home-2/png/search.png';
+import imgE from '../../assets/image/home-2/png/event-confirm.png';
+import imgR from '../../assets/image/home-2/png/right-layer.png';
 
 const Progress = ({ className, ...rest }) => {
   return (
@@ -16,7 +16,7 @@ const Progress = ({ className, ...rest }) => {
             <div className="col-xl-7 col-lg-8 col-md-9 col-xs-10">
               <div className="text-center pt-13 pt-lg-23 pb-13 pb-lg-12 pr-lg-10 pr-xl-0">
                 <h2 className="font-size-11 mb-0">
-                  Working with Instappoint is simple.
+                  Booking is truly is simple.
                 </h2>
               </div>
             </div>
@@ -56,10 +56,10 @@ const Progress = ({ className, ...rest }) => {
                     <img src={imgS} alt="" />
                   </div>
                   <div className="">
-                    <h3 className="font-size-8 mb-6">Find Leads</h3>
+                    <h3 className="font-size-8 mb-6">Pick the Duration</h3>
                     <p className="font-size-5 line-height-28 mb-0">
-                      We’ve helped over 2,500 job seekers to get into the most
-                      popular tech teams.
+                      Whether it's a quick one-hour shoot or an all-day marathon
+                      we have you covered.
                     </p>
                   </div>
                 </div>
@@ -75,10 +75,10 @@ const Progress = ({ className, ...rest }) => {
                     <img src={imgE} alt="" />
                   </div>
                   <div className="">
-                    <h3 className="font-size-8 mb-6">Book Appointments</h3>
+                    <h3 className="font-size-8 mb-6">Choose the Date</h3>
                     <p className="font-size-5 line-height-28 mb-0">
-                      We’ve helped over 2,500 job seekers to get into the most
-                      popular tech teams.
+                      Select the date that works for you. Please contact us to
+                      discuss any preferences.
                     </p>
                   </div>
                 </div>
@@ -94,10 +94,10 @@ const Progress = ({ className, ...rest }) => {
                     <img src={imgR} alt="" />
                   </div>
                   <div className="">
-                    <h3 className="font-size-8 mb-6">Get Paid</h3>
+                    <h3 className="font-size-8 mb-6">Book and Shoot</h3>
                     <p className="font-size-5 line-height-28 mb-0">
-                      We’ve helped over 2,500 job seekers to get into the most
-                      popular tech teams.
+                      Make an online payment and get ready to shoot. Have
+                      questions? We are here to help.
                     </p>
                   </div>
                 </div>
